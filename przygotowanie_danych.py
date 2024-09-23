f00d1fd0c455e93b03e05d6dbf8386e2f1fdd62d
Kacper Niezabitowski <kniezabitowski2@gmail.com>
2024-09-23 14:55:52 +0200

    Update przygotowanie_danych.py

--- przygotowanie_danych.py
+++ przygotowanie_danych.py
@@ -3,7 +3,10 @@ import pandas as pd
 import numpy as np
 
 #Wczytanie danych z pliku
-data = pd.read_csv(r"E:\BenignTraffic.pcap.csv")
-data.dropna(inplace=True)
-print(data.isnull().sum())
+data = pd.read_csv(r"C:\Users\Kacper\Desktop\BenignTraffic.pcap.csv")
+#data.dropna(inplace=True)
+#print(data.isnull().sum())
+print(data.describe())
+#data_normalization = data.copy()
+
 
