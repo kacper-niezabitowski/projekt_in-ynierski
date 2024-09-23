1afc548f287c20e40a4e618b647d3aa0f11902b9
Kacper Niezabitowski <kniezabitowski2@gmail.com>
2024-09-23 18:28:31 +0200

    Update przygotowanie_danych.py

--- przygotowanie_danych.py
+++ przygotowanie_danych.py
@@ -1,12 +1,34 @@
 #Import potrzebnych bibliotek
 import pandas as pd
 import numpy as np
+#from sklearn.ensemble import IsolationForest
 
 #Wczytanie danych z pliku
-data = pd.read_csv(r"C:\Users\Kacper\Desktop\BenignTraffic.pcap.csv")
-#data.dropna(inplace=True)
-#print(data.isnull().sum())
-print(data.describe())
-#data_normalization = data.copy()
+dane = pd.read_csv(r"E:\BenignTraffic.pcap.csv")
 
+#Usunięcie wierszy, w których występują puste dane
+dane.dropna(inplace=True) 
 
+#Sprawdzenie pustych wierszów
+print(dane.isnull().sum())
+
+#Funkcja odpowiedzialna za normalizacje danych, metoda min-max
+def min_max_normalizacja(kolumna):
+    min_wart = kolumna.min()
+    max_wart = kolumna.max()
+    return (kolumna - min_wart)/(max_wart - min_wart)
+
+#wybrane kolumny do normalizacji
+kolumny_do_normalizacji = ['Header_Length', 'Protocol Type', 'Time_To_Live', 'Rate', 'fin_flag_number'
+                         , 'syn_flag_number', 'rst_flag_number', 'psh_flag_number', 'ack_flag_number', 'ece_flag_number'
+                         , 'cwr_flag_number', 'ack_count', 'syn_count', 'fin_count', 'rst_count', 'HTTP', 'HTTPS', 'DNS'
+                         , 'Telnet', 'SMTP', 'SSH', 'IRC', 'TCP', 'UDP', 'DHCP', 'ARP', 'ICMP', 'IGMP', 'IPv', 'LLC', 'Tot sum'
+                         , 'Min', 'Max', 'AVG', 'Std', 'Tot size', 'IAT', 'Number', 'Variance']
+
+#iteracja przez każdą kolumne
+for kolumna in kolumny_do_normalizacji:
+    dane[kolumna] = min_max_normalizacja(dane[kolumna])
+
+#Zapis danych do nowego pliku
+dane.to_csv(r"E:\ZnormalizowaneDane.csv", index=False)
+print(dane.head())
